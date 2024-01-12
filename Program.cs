2591ac2244082b03d9648d11283a20178af19cc5
Emmanuella Animwaa Kumah <emmanuellaanimwaa17@icloud.com>
2024-01-12 15:55:02 +0000

    constructors

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 class Car
 {
-    public string color = "blue";
-    public string type = "toyota";
+    public string name;
+    public string color;
+
+    public Car(string name1, string color1)
+    {
+        name = name1;
+        color = color1;
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Car car1 = new Car();
-        Car car2 = new Car();
-        Console.WriteLine(car1.color);
-        Console.WriteLine(car1.type);
-        Console.WriteLine(car2.type);
+        Car Ford = new Car("Ford", "blue");
+
+        Car Toyota = new Car("Toyota", "green");
+
+        Console.WriteLine(Ford.name + " " + Ford.color);
+        Console.WriteLine(Toyota.color);
+
     }
 }
 
