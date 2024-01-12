c9e2d24abe01ac65970e5252b956e1a6396c293c
Emmanuella Animwaa Kumah <emmanuellaanimwaa17@icloud.com>
2024-01-12 14:33:56 +0000

    properties and method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace c__
+namespace ClassAndObject
 {
+
+    class Username
+    {
+        public int UserId { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public void showInfo()
+        {
+            Console.WriteLine("UserInfo");
+            Console.WriteLine("UserId : {1324}, name : {ella}, age : {19}", UserId, name, age);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yeah");
+
+            Username sl
         }
     }
 }
